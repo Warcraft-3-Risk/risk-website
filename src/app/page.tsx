36d77d1a27d550061ed41b02c922f099334c416a
Simon Nyblom <simnyb01@easv365.dk>
2025-03-27 08:31:55 +0100

    Added todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import content from "./content/content.json";
-
+//TODO: Make this more of homepage, with more content and links to other pages
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
