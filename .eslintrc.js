2770e976949f161a2c3a1d16e5ca5c9c0b01ccb6
Simon Nyblom <simnyb01@easv365.dk>
2025-03-27 12:05:55 +0100

    Added more config

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    ],
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
+  };
+  
