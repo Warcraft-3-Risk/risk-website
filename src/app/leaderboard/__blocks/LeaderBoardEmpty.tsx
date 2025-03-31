7d03e96d69015906d1ac2383b9df0486c0963ce2
Simon Nyblom <simnyb01@easv365.dk>
2025-03-31 17:11:31 +0200

    Added TODO

---
+++ src/app/leaderboard/__blocks/LeaderBoardEmpty.tsx
@@ -0,0 +1,1 @@
+//TODO Create a component for empty leaderboard
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,37 +1,37 @@
 'use client';
 
-import LeaderBoardTable from "./__blocks/LeaderBoardTable";
-import LeaderBoardFilter from "./__blocks/LeaderBoardFilter";
-import LeaderBoardSearchBar from "./__blocks/LeaderBoardSearchBar";
-import LeaderBoardPagination from "./__blocks/LeaderBoardPagination";
-import { useState } from "react";
+import LeaderBoardTable from './__blocks/table/LeaderBoardTable';
+import LeaderBoardFilter from './__blocks/table/LeaderBoardFilter';
+import LeaderBoardSearchBar from './__blocks/table/LeaderBoardSearchBar';
+import LeaderBoardPagination from './__blocks/table/LeaderBoardPagination';
+import { useState } from 'react';
 
 const dummyData = [
-  { rank: 1, name: "Player One", score: 5000 },
-  { rank: 2, name: "Player Two", score: 4500 },
-  { rank: 3, name: "Player Three", score: 4200 },
-  { rank: 4, name: "Player  Four", score: 3900 },
-  { rank: 5, name: "Player  Five", score: 3800 },
-  { rank: 6, name: "Player  Six", score: 3600 },
-  { rank: 7, name: "Player  Seven", score: 3400 },
-  { rank: 8, name: "Player  Eight", score: 3200 },
-  { rank: 9, name: "Player  Nine", score: 3000 },
-  { rank: 10, name: "Player Ten", score: 2800 },
+  { rank: 1, name: 'Player One', score: 5000 },
+  { rank: 2, name: 'Player Two', score: 4500 },
+  { rank: 3, name: 'Player Three', score: 4200 },
+  { rank: 4, name: 'Player  Four', score: 3900 },
+  { rank: 5, name: 'Player  Five', score: 3800 },
+  { rank: 6, name: 'Player  Six', score: 3600 },
+  { rank: 7, name: 'Player  Seven', score: 3400 },
+  { rank: 8, name: 'Player  Eight', score: 3200 },
+  { rank: 9, name: 'Player  Nine', score: 3000 },
+  { rank: 10, name: 'Player Ten', score: 2800 },
 ];
 
 export default function LeaderboardPage() {
-  const [filter, setFilter] = useState("highest");
-  const [searchQuery, setSearchQuery] = useState("");
+  const [filter, setFilter] = useState('highest');
+  const [searchQuery, setSearchQuery] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5);
 
   let filteredData = dummyData.filter((player) =>
-    player.name.toLowerCase().includes(searchQuery.toLowerCase())
+    player.name.toLowerCase().includes(searchQuery.toLowerCase()),
   );
 
-  if (filter === "highest") {
+  if (filter === 'highest') {
     filteredData = filteredData.sort((a, b) => b.score - a.score);
-  } else if (filter === "lowest") {
+  } else if (filter === 'lowest') {
     filteredData = filteredData.sort((a, b) => a.score - b.score);
   }
 
