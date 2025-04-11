916403acc8f3d07758572f3969b603b077d48970
Simon Nyblom <simnyb01@easv365.dk>
2025-04-11 12:00:31 +0200

    Added minor changes

--- src/app/news-and-events/page.tsx
+++ src/app/news-and-events/page.tsx
@@ -19,7 +19,7 @@ const NewsAndEventsPage = () => {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-neutral-800 p-4">
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-3xl font-bold text-black dark:text-white mb-6">
+        <h1 className="text-6xl font-bold text-black dark:text-white mb-6 justify-center flex">
           {content.newsandevents['newsandevents.title']}
         </h1>
 
