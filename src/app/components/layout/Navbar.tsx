555cf247157d79ea4c88099d92afdedf259ae9b7
Simon Nyblom <simnyb01@easv365.dk>
2025-04-23 11:17:54 +0200

    Reverting navbar change

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import { ThemeToggle } from '@/app/components/layout/ThemeToggle';
-import '@/core/SCSS/base/layout/l-navbar.scss';
 
 export function Navbar() {
   return (
-    <header className="navbg sticky top-0 z-10 backdrop-blur">
+    <header className="sticky top-0 z-10 backdrop-blur">
       <div className="flex h-14 items-center px-4">
         <div className="mr-2">
           <ThemeToggle />
