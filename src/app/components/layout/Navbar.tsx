4f748055cb2b3ef88a10640da7ab4a4dfcec39d7
Simon Nyblom <simnyb01@easv365.dk>
2025-04-23 11:12:50 +0200

    navbar fix

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { ThemeToggle } from '@/app/components/layout/ThemeToggle';
+import '@/core/SCSS/base/layout/l-navbar.scss';
 
 export function Navbar() {
   return (
-    <header className="sticky top-0 z-10 backdrop-blur">
+    <header className="navbg sticky top-0 z-10 backdrop-blur">
       <div className="flex h-14 items-center px-4">
         <div className="mr-2">
           <ThemeToggle />
