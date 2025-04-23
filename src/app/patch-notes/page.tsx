8dc6a51570c8639c67ee8b4ffe87940396353c99
Simon Nyblom <simnyb01@easv365.dk>
2025-04-23 17:29:56 +0200

    final touches

--- src/app/patch-notes/page.tsx
+++ src/app/patch-notes/page.tsx
@@ -3,16 +3,16 @@
 import { useState } from 'react';
 import PatchNotesButtons from '@/app/patch-notes/__blocks/PatchNotesButtons';
 import PatchReleaseNotes from '@/app/patch-notes/__blocks/PatchReleaseNotes';
+import PatchNotesHeader from '@/app/patch-notes/__blocks/PatchNotesHeader';
 
 export default function Page() {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <div className="background min-h-screen">
-      <div className="mx-auto px-4">
-        <h1 className="titletext text-[#f9c701] text-8xl font-bold mb-4 flex justify-center">
-          PATCH NOTES
-        </h1>
+      <PatchNotesHeader />
+
+      <div className="background mx-auto px-4">
         <PatchReleaseNotes isExpanded={isExpanded} />
         <PatchNotesButtons
           isExpanded={isExpanded}
