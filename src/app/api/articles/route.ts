027496bafcdcfd09a5e3b3de2b388a8824e4f1a2
Simon Nyblom <simnyb01@easv365.dk>
2025-04-24 12:22:45 +0200

    Firebase hosting

--- src/app/api/articles/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-import articlesData from '@/app/data/articles.json';
-
-export async function GET() {
-  return NextResponse.json(articlesData); // This returns the articles data as JSON
-}
