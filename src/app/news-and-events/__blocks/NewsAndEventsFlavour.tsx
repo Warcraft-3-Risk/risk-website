32241e323e2a728265ba8144e86bfe2e0af45b60
Simon Nyblom <simnyb01@easv365.dk>
2025-04-28 09:58:31 +0200

    more

--- src/app/news-and-events/__blocks/NewsAndEventsFlavour.tsx
+++ src/app/news-and-events/__blocks/NewsAndEventsFlavour.tsx
@@ -5,7 +5,7 @@ import '@/core/SCSS/base/layout/l-news-and-events-page.scss';
 const NewsAndEventsFlavour: React.FC = () => {
   return (
     <section className="py-8">
-      <div className="container mx-auto px-4">
+      <div className="container px-4">
         <h2 className="flavourtitle text-5xl font-bold text-left mb-4">
           {content.newsandevents['newsandevents.flavourtitle']}
         </h2>
