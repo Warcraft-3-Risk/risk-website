bc9cc44014f183c871ac35c16ad8a22766babc9b
Simon Nyblom <simnyb01@easv365.dk>
2025-04-30 09:48:04 +0200

    added scss

--- src/app/leaderboards/__blocks/LeaderBoardHeader.tsx
+++ src/app/leaderboards/__blocks/LeaderBoardHeader.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '@/core/SCSS/base/layout/l-leaderboard-header.scss';
 
 const LeaderBoardHeader: React.FC = () => {
   return (
     <header className="mb-8 text-center">
-      <h1 className="titletext text-5xl font-bold mb-2">Leaderboards</h1>
+      <h1 className="titletext text-5xl font-bold mb-2">LEADERBOARDS</h1>
       <p className="subtitle text-lg">
         Check out the top players and their rankings.
       </p>
