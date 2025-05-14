b2d10079a48bbe2b3e595457a780904a56046ac9
Simon Nyblom <simnyb01@easv365.dk>
2025-05-14 12:59:12 +0200

    Created About us and added a Stand ALone Section for the frontpage

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import content from '@/app/data/content.json';
+import '@/core/SCSS/base/layout/page/aboutus-page/p-about-us-page.scss';
+import Image from 'next/image';
 
-//TODO Make about us page, with the design made in mind
-//In your __blocks folder, that is where side relevant components go, like the header and footer etc.
-//If necessary, you can import any additional components or styles here - Create a SCSS file for the about us page and import it here
+const AboutUs = content.aboutus;
 
 const AboutPage = () => {
   return (
-    <div className="p-8 font-sans">
-      <h1 className="text-3xl font-bold mb-4">About Us</h1>
-      <p className="mb-4">
-        Welcome to our website! We are dedicated to providing the best services
-        and solutions to our customers.
-      </p>
-      <p>
-        Our team is passionate about innovation, excellence, and delivering
-        value. Thank you for visiting us!
-      </p>
+    <div className="maincontainer">
+      <div className="about-text">
+        <h2 className="">{AboutUs['subtitle']}</h2>
+        <h1 className="text-3xl mb-12">{AboutUs['title']}</h1>
+        <div className="about-description-text">
+          <p className="about-description mb-12">{AboutUs['description']}</p>
+
+          <p className="about-description-welcome">{AboutUs['welcome']}</p>
+        </div>
+      </div>
+      <div className="world">
+        <Image
+          width={1000}
+          height={1000}
+          src={'/images/World.svg'}
+          alt={''}
+        ></Image>
+      </div>
     </div>
   );
 };
