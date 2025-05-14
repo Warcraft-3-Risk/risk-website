abe4ef14b9a65614edcf6b40fd69e721089d4b71
Simon Nyblom <simnyb01@easv365.dk>
2025-05-14 13:11:12 +0200

    Started with the section

---
+++ src/app/components/layout/sections/StandAloneSection.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import content from '@/app/data/content.json';
+import '@/core/SCSS/base/_vars.scss';
+import '@/core/SCSS/base/sections/s-stand-alone-section.scss';
+
+const StandAloneContent = content.standalonesection;
+const StandAloneSection: React.FC = () => {
+  return (
+    <section className="stand-alone-section">
+      <div className="container mx-auto px-4">
+        <h2 className="titletext text-7xl font-bold text-center">
+          {StandAloneContent['standalone.title']}
+        </h2>
+        <p className="text-2xl font-semibold text-center mb-8">
+          {StandAloneContent['standalone.subtitle']}
+        </p>
+      </div>
+      <div className="container mx-auto px-4">
+        <div className="text-center">
+          <p className="text-2xl font-semibold mb-8">
+            {StandAloneContent['standalone.description']}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default StandAloneSection;
